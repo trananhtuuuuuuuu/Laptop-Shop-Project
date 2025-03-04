529ffbb2445049c0e5a9745fc5b97426a27c3377
Tran Anh Tu <trananhtu01012004@gmail.com>
2025-03-05 05:47:37 +0700

    Write hello world with spring boot

---
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -0,0 +1,20 @@
+package vn.hoidanit.laptopshop;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloController {
+  @GetMapping("/")
+  public String index(){
+    return "Hello World with Anh Tu DZ";
+  }
+}
+
+
+
+
+
+
