163fee3bf3d9b5eb4e123aae0a6d97153e56faa9
Tran Anh Tu <trananhtu01012004@gmail.com>
2025-03-05 08:11:20 +0700

    connect DB

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -1,5 +1,6 @@
 package vn.hoidanit.laptopshop;
 
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
   @GetMapping("/")
   public String index(){
-    return "Hello World with Anh Tu DZ";
+    return "Hello World with Anh Tu";
+  }
+
+
+  @GetMapping("/user")
+  public String userPage(){
+    return "This is page user access";
+  }
+
+  @GetMapping("/admin")
+  public String adminPage(){
+    return "This is page admin access";
   }
 }
 
