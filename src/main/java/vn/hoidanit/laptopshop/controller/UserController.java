3aa46171e79235a644844c7edf251214c7c9b54e
Tran Anh Tu <trananhtu01012004@gmail.com>
2025-03-05 08:46:49 +0700

    working with controller and model

---
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -0,0 +1,35 @@
+package vn.hoidanit.laptopshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import vn.hoidanit.laptopshop.service.UserService;
+
+
+
+
+@RestController
+public class UserController {
+
+  private final UserService userService;
+
+  public UserController(UserService userService) {
+    this.userService = userService;
+  }
+
+
+  // @RequestMapping("/")
+  // public String getHomePage() {
+  //     return new String();
+  // }
+  
+
+
+  @GetMapping("/")
+  public String getHomePage(){
+    return this.userService.handleHello();
+  }
+  
+}
