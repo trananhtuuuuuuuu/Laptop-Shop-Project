9177da6c45c0e5b759139c09fd452c7600b54643
Tran Anh Tu <trananhtu01012004@gmail.com>
2025-03-11 09:31:42 +0700

    UpdateCustomUserDetail

--- src/main/java/vn/hoidanit/laptopshop/controller/client/HomePageController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/client/HomePageController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import jakarta.validation.Valid;
 import vn.hoidanit.laptopshop.domain.dto.RegisterDTO;
-import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 
